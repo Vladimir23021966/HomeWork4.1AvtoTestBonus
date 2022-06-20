ee273a7efa6d537cd9e5b1facc36ada66efbffaa
Vladimir Kolyannikov <kvazitron111@gmail.com>
2022-06-20 18:32:57 +0300

    Add Unregistered user

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -18,12 +18,40 @@ public class BonusServiceTest {
     }
 
     @Test
+
+    public void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered =false;
+        long expected = 10;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку сравниваем ожидаемый и фактический:
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void shouldCalculateForRegisteredAndOverLimit() {
 
         BonusService service = new BonusService();
         // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
+        long expected = 500;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку сравниваем ожидаемый и фактический:
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForUnregisteredAndOverLimit() {
+
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
         long expected = 500;
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
